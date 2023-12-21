349bfe822239d8728768241c8cd1a4dae4bffd49
CJ-404 <cj.ptsz@yahoo.com>
2023-12-21 15:26:19 +0530

    feat:functions for load private key and public key from a file

--- Peer.java
+++ Peer.java
@@ -1,9 +1,37 @@
 import java.io.*;
 import java.net.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.security.KeyFactory;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
 
 public class Peer {
 
     final static BufferedReader consoleReader = new BufferedReader(new InputStreamReader(System.in));
+
+    // Load a private key from a file
+    private static PrivateKey loadPrivateKeyFromFile(String filePath) throws Exception {
+        Path path = Paths.get(filePath);
+        byte[] keyBytes = Files.readAllBytes(path);
+
+        PKCS8EncodedKeySpec spec = new PKCS8EncodedKeySpec(keyBytes);
+        KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+        return keyFactory.generatePrivate(spec);
+    }
+
+    // Load a public key from a file
+    private static PublicKey loadPublicKeyFromFile(String filePath) throws Exception {
+        Path path = Paths.get(filePath);
+        byte[] keyBytes = Files.readAllBytes(path);
+
+        X509EncodedKeySpec spec = new X509EncodedKeySpec(keyBytes);
+        KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+        return keyFactory.generatePublic(spec);
+    }
 
     private static String getIpAddress(int ipVersion) {
         String ipAddress = "";
